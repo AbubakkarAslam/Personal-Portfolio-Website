0732a16e0c6a98619d7f39e0dbb2bb3ff9b83bee
Muhammad Abu Bakkar Aslam <mabubakaraslam0@gmail.com>
2026-09-02 17:50:50 +0500

    update portfolio

--- src/data/skills.js
+++ src/data/skills.js
@@ -13,8 +13,8 @@ export const skillCategories = [
       { name: 'Scikit-learn', level: 85 },
       { name: 'Pandas', level: 80 },
       { name: 'NumPy', level: 80 },
-      { name: 'Matplotlibt', level: 70}
-      // { name: 'TensorFlow', level: 65 },
+      { name: 'Matplotlibt', level: 70},
+      { name: 'TensorFlow', level: 65 },
       // { name: 'PyTorch', level: 60 },
       // { name: 'OpenCV', level: 70 },
     ],
